7091b588c27de5533ce765a6a05056db20054e9b
usagi <1023192250@qq.com>
2022-05-30 20:30:04 +0800

    feat: add LauncherActivity

---
+++ app/src/main/java/top/gochiusa/originalmedia/LauncherActivity.kt
@@ -0,0 +1,10 @@
+package top.gochiusa.originalmedia
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LauncherActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
